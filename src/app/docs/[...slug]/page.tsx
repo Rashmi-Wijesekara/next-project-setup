688aa7dec97b1111689c470045c41a06140cb326
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2024-01-18 16:33:43 +0530

    catch all segments at /docs

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+export default function Docs({ params }: { params: { slug: string[] } }) {
+	
+	if (params.slug.length === 1) {
+		return <div>Docs for feature {params.slug[0]}</div>
+	}
+	else if (params.slug.length === 2) { 
+		return <div>Docs for feature {params.slug[0]} and concept {params.slug[1]}</div>
+	}
+	//return <h1>Docs home page</h1>;
+	// this will return 404 not found
+}
