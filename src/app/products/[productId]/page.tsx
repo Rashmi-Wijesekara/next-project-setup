250d4c1195c4c0f410e0d689980ed43a7d4d05d0
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2024-01-19 15:19:57 +0530

    dynamic Metadata routing

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,12 +1,30 @@
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-export default function ProductDetails({
-	params,
-}: {
+type Props = {
 	params: {
 		productId: string;
 	};
-}) {
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+	const title = await new Promise(resolve => {
+		setTimeout(() => {
+			resolve(`item ${params.productId}`);
+		}, 1000);
+	})
+
+	return {
+		title: `Product ${title}`
+	}
+	// return {
+	// 	title: `Product ${params.productId}`,
+	// }
+}
+
+export default function ProductDetails({
+	params,
+}: Props) {
 	if (Number(params.productId) > 100) {
 		notFound();
 	}
