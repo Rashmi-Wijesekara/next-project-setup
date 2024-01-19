67666cd30fe9e3dcee8d4384d293f107d9a46d5a
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2024-01-19 12:10:37 +0530

    refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "./components/footer";
-import Header from "./components/header";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 import "./globals.css";
 
 export const metadata = {
