8a9892b8023c2bba1ab72e17c2d8db25ace2be83
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2024-01-19 11:06:26 +0530

    header, footer layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
 import "./globals.css";
 
 export const metadata = {
@@ -12,7 +14,15 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<Header />
+
+				<main className="min-h-[calc(100vh-13rem)]">
+					{children}
+				</main>
+
+				<Footer />
+			</body>
 		</html>
 	);
 }
