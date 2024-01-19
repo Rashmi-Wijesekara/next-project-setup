161906f9da89d9502b28a62106218a6558fe3b8c
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2024-01-19 16:45:37 +0530

    navigate onClick

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,15 @@
+"use client"
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Products() {
+	const router = useRouter();
+
+	const redirectHandler = () => {
+		router.push('/')
+	}
+
 	return (
 		<div className="text-center">
 			<div>Products List</div>
@@ -11,6 +20,8 @@ export default function Products() {
 			<Link href={`/products/3`}>Product 3</Link>
 			<Link href={`/products/4`}>Product 4</Link>
 			</div>
+
+			<button className="bg-green-900 text-white px-5 py-2 mt-5" onClick={redirectHandler}>Redirect to Home</button>
 		</div>
 	)
 }
